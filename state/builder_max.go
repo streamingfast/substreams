982de9486d7ce54708615d9bcb2b33e56f8f3400
colindickson <colin.dickson@gmail.com>
2022-03-04 13:31:44 -0500

    builder: max tests

--- state/builder_max.go
+++ state/builder_max.go
@@ -7,67 +7,67 @@ import (
 )
 
 func (b *Builder) SetMaxBigInt(ord uint64, key string, value *big.Int) {
-	min := new(big.Int)
+	max := new(big.Int)
 	val, found := b.GetAt(ord, key)
 	if !found {
-		min = value
+		max = value
 	} else {
 		prev, _ := new(big.Int).SetString(string(val), 10)
 		if prev != nil && value.Cmp(prev) > 0 {
-			min = value
+			max = value
 		} else {
-			min = prev
+			max = prev
 		}
 	}
-	b.set(ord, key, []byte(min.String()))
+	b.set(ord, key, []byte(max.String()))
 }
 
 func (b *Builder) SetMaxInt64(ord uint64, key string, value int64) {
-	var min int64
+	var max int64
 	val, found := b.GetAt(ord, key)
 	if !found {
-		min = value
+		max = value
 	} else {
 		prev, err := strconv.ParseInt(string(val), 10, 64)
 		if err != nil || value > prev {
-			min = value
+			max = value
 		} else {
-			min = prev
+			max = prev
 		}
 	}
-	b.set(ord, key, []byte(fmt.Sprintf("%d", min)))
+	b.set(ord, key, []byte(fmt.Sprintf("%d", max)))
 }
 
 func (b *Builder) SetMaxFloat64(ord uint64, key string, value float64) {
-	var min float64
+	var max float64
 	val, found := b.GetAt(ord, key)
 	if !found {
-		min = value
+		max = value
 	} else {
 		prev, err := strconv.ParseFloat(string(val), 64)
 
 		if err != nil || value > prev {
-			min = value
+			max = value
 		} else {
-			min = prev
+			max = prev
 		}
 	}
-	b.set(ord, key, []byte(strconv.FormatFloat(min, 'g', 100, 64)))
+	b.set(ord, key, []byte(strconv.FormatFloat(max, 'g', 100, 64)))
 }
 
 func (b *Builder) SetMaxBigFloat(ord uint64, key string, value *big.Float) {
-	min := new(big.Float)
+	max := new(big.Float)
 	val, found := b.GetAt(ord, key)
 	if !found {
-		min = value
+		max = value
 	} else {
 		prev, _, err := big.ParseFloat(string(val), 10, 100, big.ToNearestEven)
 
 		if err != nil || value.Cmp(prev) > 0 {
-			min = value
+			max = value
 		} else {
-			min = prev
+			max = prev
 		}
 	}
-	b.set(ord, key, []byte(min.Text('g', -1)))
+	b.set(ord, key, []byte(max.Text('g', -1)))
 }
---
+++ state/builder_max_test.go
@@ -0,0 +1,262 @@
+package state
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"math/big"
+	"strconv"
+	"testing"
+)
+
+func TestBuilderSetMaxBigInt(t *testing.T) {
+	tests := []struct {
+		name          string
+		builder       *Builder
+		key           string
+		existingValue *big.Int
+		value         *big.Int
+		expectedValue *big.Int
+	}{
+		{
+			name:          "found less",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: big.NewInt(3),
+			value:         big.NewInt(4),
+			expectedValue: big.NewInt(4),
+		},
+		{
+			name:          "found greater",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: big.NewInt(5),
+			value:         big.NewInt(4),
+			expectedValue: big.NewInt(5),
+		},
+		{
+			name:          "not found",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: nil,
+			value:         big.NewInt(4),
+			expectedValue: big.NewInt(4),
+		},
+	}
+
+	initTestBuilder := func(b *Builder, key string, value *big.Int) {
+		b.KV = map[string][]byte{}
+		if value != nil {
+			b.KV[key] = []byte(value.String())
+		}
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			initTestBuilder(test.builder, test.key, test.existingValue)
+
+			test.builder.SetMaxBigInt(0, test.key, test.value)
+			actual, found := test.builder.GetAt(0, test.key)
+			if !found {
+				t.Errorf("value not found")
+			}
+
+			actualInt, _ := new(big.Int).SetString(string(actual), 10)
+
+			assert.Equal(t, 0, actualInt.Cmp(test.expectedValue))
+		})
+	}
+}
+
+func TestBuilderSetMaxInt64(t *testing.T) {
+	int64ptr := func(i int64) *int64 {
+		var p *int64
+		p = new(int64)
+		*p = i
+		return p
+	}
+
+	tests := []struct {
+		name          string
+		builder       *Builder
+		key           string
+		existingValue *int64
+		value         int64
+		expectedValue int64
+	}{
+		{
+			name:          "found less",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: int64ptr(3),
+			value:         4,
+			expectedValue: 4,
+		},
+		{
+			name:          "found greater",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: int64ptr(5),
+			value:         4,
+			expectedValue: 5,
+		},
+		{
+			name:          "not found",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: nil,
+			value:         4,
+			expectedValue: 4,
+		},
+	}
+
+	initTestBuilder := func(b *Builder, key string, value *int64) {
+		b.KV = map[string][]byte{}
+		if value != nil {
+			b.KV[key] = []byte(fmt.Sprintf("%d", *value))
+		}
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			initTestBuilder(test.builder, test.key, test.existingValue)
+
+			test.builder.SetMaxInt64(0, test.key, test.value)
+			actual, found := test.builder.GetAt(0, test.key)
+			if !found {
+				t.Errorf("value not found")
+			}
+
+			actualInt, err := strconv.ParseInt(string(actual), 10, 64)
+			assert.NoError(t, err)
+
+			assert.Equal(t, test.expectedValue, actualInt)
+		})
+	}
+}
+
+func TestBuilderSetMaxFloat64(t *testing.T) {
+	float64ptr := func(i float64) *float64 {
+		var p *float64
+		p = new(float64)
+		*p = i
+		return p
+	}
+
+	tests := []struct {
+		name          string
+		builder       *Builder
+		key           string
+		existingValue *float64
+		value         float64
+		expectedValue float64
+	}{
+		{
+			name:          "found less",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: float64ptr(3.0),
+			value:         4.0,
+			expectedValue: 4.0,
+		},
+		{
+			name:          "found greater",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: float64ptr(5.0),
+			value:         4.0,
+			expectedValue: 5.0,
+		},
+		{
+			name:          "not found",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: nil,
+			value:         4.0,
+			expectedValue: 4.0,
+		},
+	}
+
+	initTestBuilder := func(b *Builder, key string, value *float64) {
+		b.KV = map[string][]byte{}
+		if value != nil {
+			b.KV[key] = []byte(strconv.FormatFloat(*value, 'g', 100, 64))
+		}
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			initTestBuilder(test.builder, test.key, test.existingValue)
+
+			test.builder.SetMaxFloat64(0, test.key, test.value)
+			actual, found := test.builder.GetAt(0, test.key)
+			if !found {
+				t.Errorf("value not found")
+			}
+
+			actualInt, err := strconv.ParseFloat(string(actual), 64)
+			assert.NoError(t, err)
+
+			assert.Equal(t, test.expectedValue, actualInt)
+		})
+	}
+}
+
+func TestBuilderSetMaxBigFloat(t *testing.T) {
+	tests := []struct {
+		name          string
+		builder       *Builder
+		key           string
+		existingValue *big.Float
+		value         *big.Float
+		expectedValue *big.Float
+	}{
+		{
+			name:          "found less",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: big.NewFloat(3),
+			value:         big.NewFloat(4),
+			expectedValue: big.NewFloat(4),
+		},
+		{
+			name:          "found greater",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: big.NewFloat(5),
+			value:         big.NewFloat(4),
+			expectedValue: big.NewFloat(5),
+		},
+		{
+			name:          "not found",
+			builder:       New("b", "", "", "", nil),
+			key:           "key",
+			existingValue: nil,
+			value:         big.NewFloat(4),
+			expectedValue: big.NewFloat(4),
+		},
+	}
+
+	initTestBuilder := func(b *Builder, key string, value *big.Float) {
+		b.KV = map[string][]byte{}
+		if value != nil {
+			b.KV[key] = []byte(value.Text('g', -1))
+		}
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			initTestBuilder(test.builder, test.key, test.existingValue)
+
+			test.builder.SetMaxBigFloat(0, test.key, test.value)
+			actual, found := test.builder.GetAt(0, test.key)
+			if !found {
+				t.Errorf("value not found")
+			}
+
+			actualInt, _, err := big.ParseFloat(string(actual), 10, 100, big.ToNearestEven)
+			assert.NoError(t, err)
+
+			assert.Equal(t, 0, actualInt.Cmp(test.expectedValue))
+		})
+	}
+}

004cf4050324ec630e7e66903b01dc4acf882ab0
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-03-08 16:04:12 -0500

    fix generate.sh, generate the dummy RPC proto def

--- pb/generate.sh
+++ pb/generate.sh
@@ -19,6 +19,7 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 PROTO=${1:-"$ROOT/proto"}
 
 function main() {
+  checks
   current_dir="`pwd`"
   trap "cd \"$current_dir\"" EXIT
   pushd "$ROOT/pb" &> /dev/null
@@ -39,8 +40,35 @@ function generate() {
     fi
 
     for file in "$@"; do
-      protoc -I$PROTO $base$file --go_out=plugins=grpc,paths=source_relative:.
+      protoc -I$PROTO \
+        --go_out=. --go_opt=paths=source_relative \
+        --go-grpc_out=. --go-grpc_opt=paths=source_relative,require_unimplemented_servers=false \
+         $base$file
     done
+}
+
+function checks() {
+  # The old `protoc-gen-go` did not accept any flags. Just using `protoc-gen-go --version` in this
+  # version waits forever. So we pipe some wrong input to make it exit fast. This in the new version
+  # which supports `--version` correctly print the version anyway and discard the standard input
+  # so it's good with both version.
+  result=`printf "" | protoc-gen-go --version 2>&1 | grep -Eo v[0-9\.]+`
+  if [[ "$result" == "" ]]; then
+    echo "Your version of 'protoc-gen-go' (at `which protoc-gen-go`) is not recent enough."
+    echo ""
+    echo "To fix your problem, perform those commands:"
+    echo ""
+    echo "  pushd /tmp"
+    echo "    GO111MODULE=on go get google.golang.org/protobuf/cmd/protoc-gen-go@v1.25.0 google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1.0"
+    echo "  popd"
+    echo ""
+    echo "If everything is working as expetcted, the command:"
+    echo ""
+    echo "  protoc-gen-go --version"
+    echo ""
+    echo "Should print 'protoc-gen-go v1.25.0' (if it just hangs, you don't have the correct version)"
+    exit 1
+  fi
 }
 
 main "$@"

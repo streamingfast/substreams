25521a5a81539fa10b23b03ccc381ed215e12fa4
colindickson <colin.dickson@gmail.com>
2022-03-08 14:45:31 -0500

    builder: implement merge logic for all types (int64, bigInt, float64, big.Float)

--- state/merge.go
+++ state/merge.go
@@ -16,6 +16,8 @@ const (
 
 const (
 	OutputValueTypeInt64    = "int64"
+	OutputValueTypeFloat64  = "float64"
+	OutputValueTypeBigInt   = "bigInt"
 	OutputValueTypeBigFloat = "bigFloat"
 	OutputValueTypeString   = "string"
 )
@@ -55,15 +57,35 @@ func (b *Builder) Merge(previous *Builder) error {
 				v1 := foundOrZeroUint64(v, true)
 				latest.KV[k] = []byte(fmt.Sprintf("%d", sum(v0, v1)))
 			}
-		case OutputValueTypeBigFloat:
-			sum := func(a, b *big.Float) *big.Float {
-				return bf().Add(a, b).SetPrec(100)
+		case OutputValueTypeFloat64:
+			sum := func(a, b float64) float64 {
+				return a + b
 			}
 			for k, v := range previous.KV {
 				v0b, fv0 := latest.KV[k]
 				v0 := foundOrZeroFloat(v0b, fv0)
 				v1 := foundOrZeroFloat(v, true)
 				latest.KV[k] = []byte(floatToStr(sum(v0, v1)))
+			}
+		case OutputValueTypeBigInt:
+			sum := func(a, b *big.Int) *big.Int {
+				return bi().Add(a, b)
+			}
+			for k, v := range previous.KV {
+				v0b, fv0 := latest.KV[k]
+				v0 := foundOrZeroBigInt(v0b, fv0)
+				v1 := foundOrZeroBigInt(v, true)
+				latest.KV[k] = []byte(fmt.Sprintf("%d", sum(v0, v1)))
+			}
+		case OutputValueTypeBigFloat:
+			sum := func(a, b *big.Float) *big.Float {
+				return bf().Add(a, b).SetPrec(100)
+			}
+			for k, v := range previous.KV {
+				v0b, fv0 := latest.KV[k]
+				v0 := foundOrZeroBigFloat(v0b, fv0)
+				v1 := foundOrZeroBigFloat(v, true)
+				latest.KV[k] = []byte(bigFloatToStr(sum(v0, v1)))
 			}
 		default:
 			return fmt.Errorf("update policy %q not supported for value type %s", latest.updatePolicy, latest.valueType)
@@ -88,9 +110,9 @@ func (b *Builder) Merge(previous *Builder) error {
 
 				latest.KV[k] = []byte(fmt.Sprintf("%d", max(v0, v1)))
 			}
-		case OutputValueTypeBigFloat:
-			max := func(a, b *big.Float) *big.Float {
-				if a.Cmp(b) <= 0 {
+		case OutputValueTypeFloat64:
+			min := func(a, b float64) float64 {
+				if a < b {
 					return b
 				}
 				return a
@@ -104,7 +126,43 @@ func (b *Builder) Merge(previous *Builder) error {
 				}
 				v0 := foundOrZeroFloat(v, true)
 
-				latest.KV[k] = []byte(floatToStr(max(v0, v1)))
+				latest.KV[k] = []byte(floatToStr(min(v0, v1)))
+			}
+		case OutputValueTypeBigInt:
+			max := func(a, b *big.Int) *big.Int {
+				if a.Cmp(b) <= 0 {
+					return b
+				}
+				return a
+			}
+			for k, v := range previous.KV {
+				v1 := foundOrZeroBigInt(v, true)
+				v, found := latest.KV[k]
+				if !found {
+					latest.KV[k] = []byte(v1.String())
+					continue
+				}
+				v0 := foundOrZeroBigInt(v, true)
+
+				latest.KV[k] = []byte(fmt.Sprintf("%d", max(v0, v1)))
+			}
+		case OutputValueTypeBigFloat:
+			max := func(a, b *big.Float) *big.Float {
+				if a.Cmp(b) <= 0 {
+					return b
+				}
+				return a
+			}
+			for k, v := range previous.KV {
+				v1 := foundOrZeroBigFloat(v, true)
+				v, found := latest.KV[k]
+				if !found {
+					latest.KV[k] = []byte(bigFloatToStr(v1))
+					continue
+				}
+				v0 := foundOrZeroBigFloat(v, true)
+
+				latest.KV[k] = []byte(bigFloatToStr(max(v0, v1)))
 			}
 		default:
 			return fmt.Errorf("update policy %q not supported for value type %s", latest.updatePolicy, latest.valueType)
@@ -129,9 +187,9 @@ func (b *Builder) Merge(previous *Builder) error {
 
 				latest.KV[k] = []byte(fmt.Sprintf("%d", min(v0, v1)))
 			}
-		case OutputValueTypeBigFloat:
-			min := func(a, b *big.Float) *big.Float {
-				if a.Cmp(b) <= 0 {
+		case OutputValueTypeFloat64:
+			min := func(a, b float64) float64 {
+				if a < b {
 					return a
 				}
 				return b
@@ -146,6 +204,42 @@ func (b *Builder) Merge(previous *Builder) error {
 				v0 := foundOrZeroFloat(v, true)
 
 				latest.KV[k] = []byte(floatToStr(min(v0, v1)))
+			}
+		case OutputValueTypeBigInt:
+			min := func(a, b *big.Int) *big.Int {
+				if a.Cmp(b) <= 0 {
+					return a
+				}
+				return b
+			}
+			for k, v := range previous.KV {
+				v1 := foundOrZeroBigInt(v, true)
+				v, found := latest.KV[k]
+				if !found {
+					latest.KV[k] = []byte(v1.String())
+					continue
+				}
+				v0 := foundOrZeroBigInt(v, true)
+
+				latest.KV[k] = []byte(fmt.Sprintf("%d", min(v0, v1)))
+			}
+		case OutputValueTypeBigFloat:
+			min := func(a, b *big.Float) *big.Float {
+				if a.Cmp(b) <= 0 {
+					return a
+				}
+				return b
+			}
+			for k, v := range previous.KV {
+				v1 := foundOrZeroBigFloat(v, true)
+				v, found := latest.KV[k]
+				if !found {
+					latest.KV[k] = []byte(bigFloatToStr(v1))
+					continue
+				}
+				v0 := foundOrZeroBigFloat(v, true)
+
+				latest.KV[k] = []byte(bigFloatToStr(min(v0, v1)))
 			}
 		default:
 			return fmt.Errorf("update policy %q not supported for value type %s", latest.updatePolicy, latest.valueType)
@@ -169,14 +263,33 @@ func foundOrZeroUint64(in []byte, found bool) uint64 {
 	return uint64(val)
 }
 
-func foundOrZeroFloat(in []byte, found bool) *big.Float {
+func foundOrZeroBigFloat(in []byte, found bool) *big.Float {
 	if !found {
 		return bf()
 	}
-	return bytesToFloat(in)
+	return bytesToBigFloat(in)
 }
 
-func strToFloat(in string) *big.Float {
+func foundOrZeroBigInt(in []byte, found bool) *big.Int {
+	if !found {
+		return bi()
+	}
+	return bytesToBigInt(in)
+}
+
+func foundOrZeroFloat(in []byte, found bool) float64 {
+	if !found {
+		return float64(0)
+	}
+
+	f, err := strconv.ParseFloat(string(in), 64)
+	if err != nil {
+		return float64(0)
+	}
+	return f
+}
+
+func strToBigFloat(in string) *big.Float {
 	newFloat, _, err := big.ParseFloat(in, 10, 100, big.ToNearestEven)
 	if err != nil {
 		panic(fmt.Sprintf("cannot load float %q: %s", in, err))
@@ -184,16 +297,46 @@ func strToFloat(in string) *big.Float {
 	return newFloat.SetPrec(100)
 }
 
-func bytesToFloat(in []byte) *big.Float {
-	return strToFloat(string(in))
+func strToFloat(in string) float64 {
+	newFloat, _, err := big.ParseFloat(in, 10, 100, big.ToNearestEven)
+	if err != nil {
+		panic(fmt.Sprintf("cannot load float %q: %s", in, err))
+	}
+	f, _ := newFloat.SetPrec(100).Float64()
+	return f
 }
 
-func floatToStr(f *big.Float) string {
-	return f.Text('g', -1)
+func strToBigInt(in string) *big.Int {
+	i64, err := strconv.ParseInt(in, 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("cannot load int %q: %s", in, err))
+	}
+	return big.NewInt(i64)
 }
 
-func floatToBytes(f *big.Float) []byte {
+func bytesToBigFloat(in []byte) *big.Float {
+	return strToBigFloat(string(in))
+}
+
+func bytesToBigInt(in []byte) *big.Int {
+	return strToBigInt(string(in))
+}
+
+func floatToStr(f float64) string {
+	return big.NewFloat(f).Text('g', -1)
+}
+
+func floatToBytes(f float64) []byte {
 	return []byte(floatToStr(f))
 }
 
+func bigFloatToStr(f *big.Float) string {
+	return f.Text('g', -1)
+}
+
+func bigFloatToBytes(f *big.Float) []byte {
+	return []byte(bigFloatToStr(f))
+}
+
 var bf = func() *big.Float { return new(big.Float).SetPrec(100) }
+var bi = func() *big.Int { return new(big.Int) }

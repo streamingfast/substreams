7a54d744f033f1a1f9aa42a5ac4565c95a7d470b
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-03-09 17:56:42 -0500

    return pointer when eth_call

--- rust/substreams/src/rpc.rs
+++ rust/substreams/src/rpc.rs
@@ -1,9 +1,10 @@
 use crate::externs;
 use crate::memory::memory;
 
-pub fn eth_call(input: Vec<u8>) {
+pub fn eth_call(input: Vec<u8>) -> *mut u8 {
     unsafe {
         let rpc_response_ptr = memory::alloc(8);
-        return externs::rpc::eth_call(input.as_ptr(), input.len() as u32, rpc_response_ptr);
+        externs::rpc::eth_call(input.as_ptr(), input.len() as u32, rpc_response_ptr);
+        return rpc_response_ptr
     }
 }

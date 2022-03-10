4d6c27879236061a2b646c0fa9ec6b16bb0df220
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-03-09 23:14:43 -0500

    fix rpc eth_call to return output data not pointer

--- rust/substreams/src/rpc.rs
+++ rust/substreams/src/rpc.rs
@@ -1,10 +1,10 @@
 use crate::externs;
 use crate::memory::memory;
 
-pub fn eth_call(input: Vec<u8>) -> *mut u8 {
+pub fn eth_call(input: Vec<u8>) -> Vec<u8> {
     unsafe {
         let rpc_response_ptr = memory::alloc(8);
         externs::rpc::eth_call(input.as_ptr(), input.len() as u32, rpc_response_ptr);
-        return rpc_response_ptr
+        return memory::get_output_data(rpc_response_ptr);
     }
 }

9bec0c5427685fbdfa6f41dd57654879af209683
billettc <charles@cbillette.com>
2022-03-29 09:12:40 -0400

    small signature refactoring

--- manifest/signature.go
+++ manifest/signature.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha1"
 	"encoding/binary"
 	"fmt"
-	"sort"
 
 	pbtransform "github.com/streamingfast/substreams/pb/sf/substreams/transform/v1"
 )
@@ -17,8 +16,10 @@ func SignModule(manifest *pbtransform.Manifest, module *pbtransform.Module, grap
 
 	startBlockBytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(startBlockBytes, *module.StartBlock) //at this point start block should have been resolved
+	buf.WriteString("start_block")
 	buf.Write(startBlockBytes)
 
+	buf.WriteString("kind")
 	switch module.Kind.(type) {
 	case *pbtransform.Module_KindMap:
 		buf.WriteString("map")
@@ -28,6 +29,7 @@ func SignModule(manifest *pbtransform.Manifest, module *pbtransform.Module, grap
 		panic(fmt.Sprint("invalid module file %T", module.Kind))
 	}
 
+	buf.WriteString("code")
 	switch m := module.Code.(type) {
 	case *pbtransform.Module_WasmCode:
 		code := manifest.ModulesCode[m.WasmCode.Index]
@@ -35,15 +37,12 @@ func SignModule(manifest *pbtransform.Manifest, module *pbtransform.Module, grap
 		buf.WriteString(m.WasmCode.Entrypoint)
 	}
 
-	sort.Slice(module.Inputs, func(i, j int) bool {
-		iName := inputName(module.Inputs[i])
-		jName := inputName(module.Inputs[j])
-		return iName < jName
-	})
+	buf.WriteString("inputs")
 	for _, input := range module.Inputs {
 		buf.WriteString(inputName(input))
 	}
 
+	buf.WriteString("ancestors")
 	ancestors, _ := graph.AncestorsOf(module.Name)
 	for _, ancestor := range ancestors {
 		sig := SignModule(manifest, ancestor, graph)

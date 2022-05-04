9d2239aa6ceb7cc826d7aa9641cd010edaab075e
colindickson <colin.dickson@gmail.com>
2022-05-04 16:29:38 -0400

    cleaup tests

--- state/store_test.go
+++ state/store_test.go
@@ -1,35 +1,11 @@
 package state
 
 import (
-	"context"
 	"github.com/stretchr/testify/require"
 	"testing"
-
-	"github.com/streamingfast/dstore"
 )
 
-type TestStore struct {
-	*dstore.MockStore
-
-	WriteStateFunc        func(ctx context.Context, content []byte, blockNum uint64) error
-	WritePartialStateFunc func(ctx context.Context, content []byte, startBlockNum, endBlockNum uint64) error
-}
-
-func (io *TestStore) WritePartialState(ctx context.Context, content []byte, startBlockNum, endBlockNum uint64) error {
-	if io.WritePartialStateFunc != nil {
-		return io.WritePartialStateFunc(ctx, content, startBlockNum, endBlockNum)
-	}
-	return nil
-}
-
-func (io *TestStore) WriteState(ctx context.Context, content []byte, blockNum uint64) error {
-	if io.WriteStateFunc != nil {
-		return io.WriteStateFunc(ctx, content, blockNum)
-	}
-	return nil
-}
-
-func TestStateFileName(t *testing.T) {
+func TestFileName(t *testing.T) {
 	prefix := StateFilePrefix("test", 10000)
 	require.Equal(t, "test-0000010000", prefix)
 

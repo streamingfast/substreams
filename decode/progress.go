5c86a8f9da1889844ec4f919d304b640db4e600d
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-05-27 10:58:23 -0400

    removing bar thingy

--- decode/progress.go
+++ decode/progress.go
@@ -19,8 +19,6 @@ type Bar struct {
 	percent uint64 // progress percentage
 	start   uint64 // starting point for progress
 	cur     uint64 // current progress
-	rate    string // the actual progress bar to be printed
-	graph   string // the fill value for progress bar
 }
 
 func (bar *Bar) NewOption(cur, blockRangeStartBlock, blockRangeEndBlock uint64) {
@@ -30,30 +28,17 @@ func (bar *Bar) NewOption(cur, blockRangeStartBlock, blockRangeEndBlock uint64) 
 	bar.BlockRangeEndBlock = blockRangeEndBlock
 
 	bar.cur = cur
-	bar.graph = "#"
 	bar.percent = bar.getPercent()
-	for i := 0; i < int(bar.percent); i += 2 {
-		bar.rate += bar.graph // initial progress position
-	}
 }
 
 func (bar *Bar) Play(cur uint64, moduleName string) {
 	bar.cur = cur
-	last := bar.percent
 	bar.percent = bar.getPercent()
-	padding := uint64(50) // size of [   ]
-	_ = padding
-	if bar.percent != last && bar.percent%2 == 0 {
-		bar.rate += bar.graph
-	}
 
 	fmt.Printf("\r Executing: %s to catch up for blocks %d - %d -- %3d%% %8d/%d",
-		//fmt.Printf("\r Executing: %s to catch up for blocks %d - %d [%-*s]%3d%% %8d/%d",
 		moduleName,
 		bar.BlockRangeStartBlock,
 		bar.BlockRangeEndBlock,
-		//padding,
-		//bar.rate,
 		bar.percent,
 		bar.cur,
 		bar.Total)
@@ -65,7 +50,6 @@ func (bar *Bar) Finish() {
 	bar.percent = 0
 	bar.start = 0
 	bar.Total = 0
-	bar.rate = ""
 	bar.BlockRangeStartBlock = 0
 	bar.BlockRangeEndBlock = 0
 }

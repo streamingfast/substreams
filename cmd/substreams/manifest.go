9dca46bc9fa9f82ebcbdaf4a309fba40b1a1ebff
Julien Cassis <juliencassis@gmail.com>
2022-05-30 10:29:45 -0400

    Fix manifest info formatting

--- cmd/substreams/manifest.go
+++ cmd/substreams/manifest.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/streamingfast/substreams/manifest"
+	pbsubstreams "github.com/streamingfast/substreams/pb/sf/substreams/v1"
 )
 
 var manifestCmd = &cobra.Command{
@@ -50,11 +51,24 @@ func runManifestInfo(cmd *cobra.Command, args []string) error {
 
 	fmt.Println("Description:", pkg.PackageMeta[0].Doc)
 	fmt.Println("Version:", pkg.PackageMeta[0].Version)
+	fmt.Println("Modules:")
 	fmt.Println("----")
 	for _, module := range pkg.Modules.Modules {
-		fmt.Println("module:", module.Name)
-		fmt.Println("Kind:", module.GetKind())
+		fmt.Println("Name:", module.Name)
+		kind := module.GetKind()
+		switch v := kind.(type) {
+		case *pbsubstreams.Module_KindMap_:
+			fmt.Println("Kind: Map")
+			fmt.Println("Output Type: ", v.KindMap.OutputType)
+		case *pbsubstreams.Module_KindStore_:
+			fmt.Println("Kind: Store")
+			fmt.Println("Value Type: ", v.KindStore.ValueType)
+			fmt.Println("Update Policy: ", v.KindStore.UpdatePolicy)
+		default:
+			fmt.Println("Kind: Unknown")
+		}
 		fmt.Println("Hash:", manifest.HashModuleAsString(pkg.Modules, graph, module))
+		fmt.Println("")
 	}
 
 	return nil

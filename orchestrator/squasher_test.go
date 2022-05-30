f1f7352e9ddaa9828dd845291647ab4482e98de9
colindickson <colin.dickson@gmail.com>
2022-05-30 19:46:32 -0400

    orchestrator: concurrnt squasher test

--- orchestrator/squasher_test.go
+++ orchestrator/squasher_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"sync"
 	"testing"
 
 	"github.com/streamingfast/dstore"
@@ -87,4 +88,92 @@ func testStateBuilder(store dstore.Store) *state.Builder {
 		UpdatePolicy: pbsubstreams.Module_KindStore_UPDATE_POLICY_SET,
 		ValueType:    state.OutputValueTypeString,
 	}
+}
+
+func TestConcurrentSquasherClose(t *testing.T) {
+	var writeLock sync.RWMutex
+	var infoBytes []byte
+	var writeCount int
+
+	store := dstore.NewMockStore(nil)
+	store.WriteObjectFunc = func(ctx context.Context, base string, f io.Reader) error {
+		writeLock.Lock()
+		defer writeLock.Unlock()
+
+		if base == state.InfoFileName() {
+			infoBytes, _ = io.ReadAll(f)
+			return nil
+		}
+
+		writeCount++
+
+		return nil
+	}
+
+	store.OpenObjectFunc = func(ctx context.Context, name string) (out io.ReadCloser, err error) {
+		if name == state.InfoFileName() {
+			writeLock.RLock()
+			defer writeLock.RUnlock()
+			if infoBytes == nil {
+				return nil, dstore.ErrNotFound
+			}
+			return io.NopCloser(bytes.NewReader(infoBytes)), nil
+		}
+
+		return nil, fmt.Errorf("no")
+	}
+
+	var s1 *Squasher
+	s1 = &Squasher{
+		squashables: map[string]*Squashable{
+			"testBuilder": &Squashable{
+				builder: testStateBuilder(store),
+				ranges:  []*block.Range{},
+			},
+		},
+		storeSaveInterval: 10_000,
+		notifier:          nil,
+	}
+
+	var s2 *Squasher
+	s2 = &Squasher{
+		squashables: map[string]*Squashable{
+			"testBuilder": &Squashable{
+				builder: testStateBuilder(store),
+				ranges:  []*block.Range{},
+			},
+		},
+		storeSaveInterval: 10_000,
+		notifier:          nil,
+	}
+
+	var errClose1 error
+	var errClose2 error
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		err := s1.Squash(context.Background(), "testBuilder", &block.Range{StartBlock: 10_000, ExclusiveEndBlock: 20_000})
+		if err != nil {
+			t.Fail()
+		}
+		errClose1 = s1.Close()
+	}()
+
+	go func() {
+		defer wg.Done()
+		err := s2.Squash(context.Background(), "testBuilder", &block.Range{StartBlock: 10_000, ExclusiveEndBlock: 20_000})
+		if err != nil {
+			t.Fail()
+		}
+		errClose2 = s2.Close()
+	}()
+
+	wg.Wait()
+
+	require.Nil(t, errClose1)
+	require.Nil(t, errClose2)
+	require.Equal(t, 2, writeCount)
 }

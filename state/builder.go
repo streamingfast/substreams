941dfe50db09d15c832b8137ad3d7a11c020cbab
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-06-01 16:07:01 -0400

    small fix on kv getting reinitialized on each new squasher for each store

--- state/builder.go
+++ state/builder.go
@@ -117,7 +117,7 @@ func (b *Store) Clone(newStoreStartBlock uint64) *Store {
 		ModuleInitialBlock: b.ModuleInitialBlock,
 		StoreInitialBlock:  newStoreStartBlock,
 		ModuleHash:         b.ModuleHash,
-		KV:                 map[string][]byte{},
+		KV:                 b.KV,
 		Deltas:             []*pbsubstreams.StoreDelta{},
 		UpdatePolicy:       b.UpdatePolicy,
 		ValueType:          b.ValueType,

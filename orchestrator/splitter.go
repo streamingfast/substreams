10a297809735b9d87b3d776b58544a84ebf83bbd
colindickson <colin.dickson@gmail.com>
2022-06-14 15:38:32 -0400

    orchestrator: refactorings

--- orchestrator/splitter.go
+++ orchestrator/splitter.go
@@ -1,19 +1,14 @@
 package orchestrator
 
 import (
-	"fmt"
-	"strings"
-
-	"go.uber.org/zap/zapcore"
-
 	"github.com/streamingfast/substreams/block"
 	pbsubstreams "github.com/streamingfast/substreams/pb/sf/substreams/v1"
 )
 
 type WorkPlan map[string]*WorkUnit
 
-func (plan WorkPlan) ProgressMessages() (out []*pbsubstreams.ModuleProgress) {
-	for storeName, unit := range plan {
+func (p WorkPlan) ProgressMessages() (out []*pbsubstreams.ModuleProgress) {
+	for storeName, unit := range p {
 		if unit.completedRange == nil {
 			continue
 		}
@@ -46,8 +41,8 @@ type WorkUnit struct {
 	RequestRanges       block.Ranges
 }
 
-func (work *WorkUnit) InitialProcessedPartials() block.Ranges {
-	return work.partialsPresent.Merged()
+func (w *WorkUnit) InitialProcessedPartials() block.Ranges {
+	return w.partialsPresent.Merged()
 }
 
 func SplitWork(modName string, subRequestSlipSize, modInitBlock, incomingReqStartBlock uint64, snapshots *Snapshots) (work *WorkUnit) {
@@ -73,6 +68,13 @@ func SplitWork(modName string, subRequestSlipSize, modInitBlock, incomingReqStar
 		return
 	}
 
+	minOf := func(a, b uint64) uint64 {
+		if a < b {
+			return a
+		}
+		return b
+	}
+
 	for ptr := backProcessStartBlock; ptr < incomingReqStartBlock; {
 		end := minOf(ptr-ptr%subRequestSlipSize+subRequestSlipSize, incomingReqStartBlock)
 		newPartial := block.NewRange(ptr, end)
@@ -85,45 +87,4 @@ func SplitWork(modName string, subRequestSlipSize, modInitBlock, incomingReqStar
 	}
 	work.RequestRanges = work.partialsMissing.MergeRanges(work.subRequestSplitSize)
 	return work
-}
-
-func minOf(a, b uint64) uint64 {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-type chunks []*chunk
-
-func (c chunks) String() string {
-	var sc []string
-	for _, s := range c {
-		var add string
-		if s.tempPartial {
-			add = "TMP:"
-		}
-		sc = append(sc, fmt.Sprintf("%s%d-%d", add, s.start, s.end))
-	}
-	return strings.Join(sc, ", ")
-}
-
-type chunk struct {
-	start       uint64
-	end         uint64 // exclusive end
-	tempPartial bool   // for off-of-bound stores (like ending in 1123, and not on 1000)
-}
-
-func (c *chunk) String() string {
-	var add string
-	if c.tempPartial {
-		add = "TMP:"
-	}
-	return fmt.Sprintf("%s%d-%d", add, c.start, c.end)
-}
-func (c *chunk) MarshalLogObject(enc zapcore.ObjectEncoder) error {
-	enc.AddUint64("start_block", c.start)
-	enc.AddUint64("end_block", c.end)
-
-	return nil
 }

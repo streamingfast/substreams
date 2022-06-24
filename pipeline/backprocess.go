3d59efbd48ca7ac625257c90e9e1347d8c71fe1a
Alexandre Bourget <alex@streamingfast.io>
2022-06-23 23:08:39 -0400

    Don't run if the snapshots weren't found. Can't work with a `nil` pointer anyway.

--- pipeline/backprocess.go
+++ pipeline/backprocess.go
@@ -35,7 +35,10 @@ func (p *Pipeline) backProcessStores(
 
 	workPlan := orchestrator.WorkPlan{}
 	for _, mod := range p.storeModules {
-		snapshot := storageState.Snapshots[mod.Name]
+		snapshot, found := storageState.Snapshots[mod.Name]
+		if !found {
+			return nil, fmt.Errorf("error: snapshots nil for module %q", mod.Name)
+		}
 		if workUnit := orchestrator.SplitWork(mod.Name, p.storeSaveInterval, mod.InitialBlock, uint64(p.request.StartBlockNum), snapshot); workUnit != nil {
 			workPlan[mod.Name] = workUnit
 		}

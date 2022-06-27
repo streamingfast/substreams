c06d3b6de3c606a2baf2b1b3a28862c1efd1ce06
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-06-27 14:20:27 -0400

    adding check for the splitwork to make sure we have some work todo

--- pipeline/backprocess.go
+++ pipeline/backprocess.go
@@ -35,10 +35,10 @@ func (p *Pipeline) backProcessStores(
 
 	workPlan := orchestrator.WorkPlan{}
 	for _, mod := range p.storeModules {
-
-		snapshot := storageState.Snapshots[mod.Name]
-		if workUnit := orchestrator.SplitWork(mod.Name, p.storeSaveInterval, mod.InitialBlock, uint64(p.request.StartBlockNum), snapshot); workUnit != nil {
-			workPlan[mod.Name] = workUnit
+		if snapshot, ok := storageState.Snapshots[mod.Name]; ok {
+			if workUnit := orchestrator.SplitWork(mod.Name, p.storeSaveInterval, mod.InitialBlock, uint64(p.request.StartBlockNum), snapshot); workUnit != nil {
+				workPlan[mod.Name] = workUnit
+			}
 		}
 	}
 

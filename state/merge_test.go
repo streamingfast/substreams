215313c5de60ab09faf46752d26759d8514599ee
colindickson <colin.dickson@gmail.com>
2022-08-16 18:43:33 -0400

    more cleanup of builder keyword

--- state/merge_test.go
+++ state/merge_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBuilder_Merge(t *testing.T) {
+func TestStore_Merge(t *testing.T) {
 	tests := []struct {
 		name            string
 		latest          *Store

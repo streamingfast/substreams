67c096aee82d113ebd1da29fe1fd92d5d992a9db
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-08-16 18:55:08 -0400

    adding RetryableErr concept if the error is considered a RetryableErr and to retry on the other side of the scheduler.go and returning a normal error when it is considered an unRetryableErr

--- orchestrator/workerpool.go
+++ orchestrator/workerpool.go
@@ -107,6 +107,14 @@ type Worker struct {
 	grpcClientFactory substreams.GrpcClientFactory
 }
 
+type RetryableErr struct {
+	cause error
+}
+
+func (r *RetryableErr) Error() string {
+	return r.cause.Error()
+}
+
 func (w *Worker) Run(ctx context.Context, job *Job, jobStats map[*Job]*JobStat, requestModules *pbsubstreams.Modules, respFunc substreams.ResponseFunc) ([]*block.Range, error) {
 	start := time.Now()
 
@@ -114,7 +122,7 @@ func (w *Worker) Run(ctx context.Context, job *Job, jobStats map[*Job]*JobStat, 
 	grpcClient, connClose, grpcCallOpts, err := w.grpcClientFactory(ctx)
 	if err != nil {
 		jobLogger.Error("getting grpc client", zap.Error(err))
-		return nil, fmt.Errorf("grpc client factory: %w", err)
+		return nil, &RetryableErr{cause: fmt.Errorf("grpc client factory: %w", err)}
 	}
 	defer connClose()
 
@@ -125,7 +133,7 @@ func (w *Worker) Run(ctx context.Context, job *Job, jobStats map[*Job]*JobStat, 
 	stream, err := grpcClient.Blocks(ctx, request, grpcCallOpts...)
 	if err != nil {
 		jobLogger.Error("getting block stream", zap.Error(err))
-		return nil, fmt.Errorf("getting block stream: %w", err)
+		return nil, &RetryableErr{cause: fmt.Errorf("getting block stream: %w", err)}
 	}
 	defer func() {
 		stream.CloseSend()
@@ -160,11 +168,44 @@ func (w *Worker) Run(ctx context.Context, job *Job, jobStats map[*Job]*JobStat, 
 		select {
 		case <-ctx.Done():
 			jobLogger.Warn("context cancel will waiting for stream data, worker is terminating")
-			return nil, ctx.Err()
+			return nil, &RetryableErr{cause: ctx.Err()}
 		default:
 		}
 
 		resp, err := stream.Recv()
+		if resp != nil {
+			switch r := resp.Message.(type) {
+			case *pbsubstreams.Response_Progress:
+				err := respFunc(resp)
+				if err != nil {
+					jobLogger.Warn("worker done on respFunc error", zap.Error(err))
+					return nil, &RetryableErr{cause: fmt.Errorf("sending progress: %w", err)}
+				}
+
+				for _, progress := range resp.GetProgress().Modules {
+					if f := progress.GetFailed(); f != nil {
+						zlog.Debug("failed execution of substreams", zap.String("reason", f.Reason))
+						return nil, fmt.Errorf("module %s failed on host: %s", progress.Name, f.Reason)
+					}
+				}
+
+				if len(resp.GetProgress().Modules) > 0 {
+					module := resp.GetProgress().Modules[0]
+					if rangeCount := len(module.GetProcessedRanges().ProcessedRanges); rangeCount > 0 {
+						endBlock := module.GetProcessedRanges().ProcessedRanges[rangeCount-1].EndBlock
+						jobStat.update(endBlock)
+					}
+				}
+
+			case *pbsubstreams.Response_SnapshotData:
+				_ = r.SnapshotData
+			case *pbsubstreams.Response_SnapshotComplete:
+				_ = r.SnapshotComplete
+			case *pbsubstreams.Response_Data:
+				// These are not returned by virtue of `returnOutputs`
+			}
+		}
+
 		if err != nil {
 			if err == io.EOF {
 				jobLogger.Info("worker done", zap.Object("job", job))
@@ -177,37 +218,7 @@ func (w *Worker) Run(ctx context.Context, job *Job, jobStats map[*Job]*JobStat, 
 				return partialsWritten, nil
 			}
 			jobLogger.Warn("worker done on stream error", zap.Error(err))
-			return nil, fmt.Errorf("receiving stream resp: %w", err)
-		}
-
-		switch r := resp.Message.(type) {
-		case *pbsubstreams.Response_Progress:
-
-			err := respFunc(resp)
-			if err != nil {
-				jobLogger.Warn("worker done on respFunc error", zap.Error(err))
-				return nil, fmt.Errorf("sending progress: %w", err)
-			}
-
-			for _, progress := range resp.GetProgress().Modules {
-				if f := progress.GetFailed(); f != nil {
-					return nil, fmt.Errorf("module %s failed on host: %s", progress.Name, f.Reason)
-				}
-			}
-			if len(resp.GetProgress().Modules) > 0 {
-				module := resp.GetProgress().Modules[0]
-				if rangeCount := len(module.GetProcessedRanges().ProcessedRanges); rangeCount > 0 {
-					endBlock := module.GetProcessedRanges().ProcessedRanges[rangeCount-1].EndBlock
-					jobStat.update(endBlock)
-				}
-			}
-
-		case *pbsubstreams.Response_SnapshotData:
-			_ = r.SnapshotData
-		case *pbsubstreams.Response_SnapshotComplete:
-			_ = r.SnapshotComplete
-		case *pbsubstreams.Response_Data:
-			// These are not returned by virtue of `returnOutputs`
+			return nil, &RetryableErr{cause: fmt.Errorf("receiving stream resp: %w", err)}
 		}
 	}
 }

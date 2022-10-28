bb2549d7e819fae1f00555e585f8c0cc3db607bb
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-10-28 14:19:18 -0400

    adding tests for resolve start block

--- reqctx/context_test.go
+++ reqctx/context_test.go
@@ -3,57 +3,99 @@ package reqctx
 import (
 	"testing"
 
+	"github.com/streamingfast/bstream"
+
 	pbsubstreams "github.com/streamingfast/substreams/pb/sf/substreams/v1"
 )
 
-func Test_ResolveStartBlockNumStepNew(t *testing.T) {
-	testCases := struct {
+func Test_resolveStartBlockNum(t *testing.T) {
+	tests := []struct {
 		name             string
 		req              *pbsubstreams.Request
 		expectedBlockNum uint64
-	}{}
-	_ = testCases
+		wantErr          bool
+	}{
+		{
+			name: "invalid cursor step",
+			req: &pbsubstreams.Request{
+				StartBlockNum: 10,
+				StartCursor: (&bstream.Cursor{
+					Step:      bstream.StepType(0),
+					Block:     bstream.NewBlockRef("10a", 10),
+					LIB:       bstream.NewBlockRef("9a", 9),
+					HeadBlock: bstream.NewBlockRef("10a", 10),
+				}).ToOpaque(),
+			},
+			expectedBlockNum: 0,
+			wantErr:          true,
+		},
+		{
+			name: "step undo",
+			req: &pbsubstreams.Request{
+				StartBlockNum: 10,
+				StartCursor: (&bstream.Cursor{
+					Step:      bstream.StepUndo,
+					Block:     bstream.NewBlockRef("10a", 10),
+					LIB:       bstream.NewBlockRef("9a", 9),
+					HeadBlock: bstream.NewBlockRef("10a", 10),
+				}).ToOpaque(),
+			},
+			expectedBlockNum: 10,
+			wantErr:          false,
+		},
+		{
+			name: "step new",
+			req: &pbsubstreams.Request{
+				StartBlockNum: 10,
+				StartCursor: (&bstream.Cursor{
+					Step:      bstream.StepNew,
+					Block:     bstream.NewBlockRef("10a", 10),
+					LIB:       bstream.NewBlockRef("9a", 9),
+					HeadBlock: bstream.NewBlockRef("10a", 10),
+				}).ToOpaque(),
+			},
+			expectedBlockNum: 11,
+			wantErr:          false,
+		},
+		{
+			name: "step irreversible",
+			req: &pbsubstreams.Request{
+				StartBlockNum: 10,
+				StartCursor: (&bstream.Cursor{
+					Step:      bstream.StepIrreversible,
+					Block:     bstream.NewBlockRef("10a", 10),
+					LIB:       bstream.NewBlockRef("9a", 9),
+					HeadBlock: bstream.NewBlockRef("10a", 10),
+				}).ToOpaque(),
+			},
+			expectedBlockNum: 11,
+			wantErr:          false,
+		},
+		{
+			name: "step new irreversible",
+			req: &pbsubstreams.Request{
+				StartBlockNum: 10,
+				StartCursor: (&bstream.Cursor{
+					Step:      bstream.StepNewIrreversible,
+					Block:     bstream.NewBlockRef("10a", 10),
+					LIB:       bstream.NewBlockRef("9a", 9),
+					HeadBlock: bstream.NewBlockRef("10a", 10),
+				}).ToOpaque(),
+			},
+			expectedBlockNum: 11,
+			wantErr:          false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := resolveStartBlockNum(tt.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("resolveStartBlockNum() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.expectedBlockNum {
+				t.Errorf("resolveStartBlockNum() got = %v, want %v", got, tt.expectedBlockNum)
+			}
+		})
+	}
 }
-
-// todo:
-//func Test_SimpleMapModuleWithCursor(t *testing.T) {
-//	newBlockGenerator := func(startBlock uint64, inclusiveStopBlock uint64) TestBlockGenerator {
-//		return &LinearBlockGenerator{
-//			startBlock:         startBlock,
-//			inclusiveStopBlock: inclusiveStopBlock,
-//		}
-//	}
-//	cursor := &bstream.Cursor{
-//		Step:      bstream.StepNew,
-//		Block:     bstream.NewBlockRef("10a", 10),
-//		LIB:       bstream.NewBlockRef("9a", 9),
-//		HeadBlock: bstream.NewBlockRef("10a", 10),
-//	}
-//	moduleOutputs, err := runTest(t, cursor, 10, 12, []string{"test_map"}, 10, newBlockGenerator, nil)
-//	require.NoError(t, err)
-//	require.Equal(t, []string{
-//		//		`{"name":"test_map","result":{"block_number":10,"block_hash":"block-10"}}`, // we already saw block 10, it is the cursor
-//		`{"name":"test_map","result":{"block_number":11,"block_hash":"block-11"}}`,
-//	}, moduleOutputs)
-//}
-//
-//func Test_SimpleMapModuleWithCursorUndo(t *testing.T) {
-//	newBlockGenerator := func(startBlock uint64, inclusiveStopBlock uint64) TestBlockGenerator {
-//		return &LinearBlockGenerator{
-//			startBlock:         startBlock,
-//			inclusiveStopBlock: inclusiveStopBlock,
-//		}
-//	}
-//	cursor := &bstream.Cursor{
-//		Step:      bstream.StepUndo,
-//		Block:     bstream.NewBlockRef("10a", 10),
-//		LIB:       bstream.NewBlockRef("8a", 8),
-//		HeadBlock: bstream.NewBlockRef("10a", 10),
-//	}
-//	moduleOutputs, err := runTest(t, cursor, 10, 12, []string{"test_map"}, 10, newBlockGenerator, nil)
-//	require.NoError(t, err)
-//	require.Equal(t, []string{
-//		`{"name":"test_map","result":{"block_number":10,"block_hash":"block-10"}}`, // customer has "undone" block 10, need to send it again
-//		`{"name":"test_map","result":{"block_number":11,"block_hash":"block-11"}}`,
-//	}, moduleOutputs)
-//}

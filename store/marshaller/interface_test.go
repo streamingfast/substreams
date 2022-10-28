94eaadedd395f68c3ceebb2bcdec737f9d6df595
Julien Cassis <juliencassis@gmail.com>
2022-10-28 12:04:59 -0400

    Cleaned up benchmarking marshaller test

--- store/marshaller/interface_test.go
+++ store/marshaller/interface_test.go
@@ -10,6 +10,15 @@ import (
 	"go.uber.org/atomic"
 )
 
+var marshallers = []struct {
+	name string
+	m    Marshaller
+}{
+	{"binary", &Binary{}},
+	{"proto", &Proto{}},
+	{"ProtoingFast", &ProtoingFast{}},
+}
+
 func Benchmark_Marshall(b *testing.B) {
 	counter := atomic.NewUint64(0)
 	keyGen := func() string {
@@ -22,9 +31,9 @@ func Benchmark_Marshall(b *testing.B) {
 		return fmt.Sprintf("total:%x:%x", address1, address2)
 	}
 
-	for _, m := range []Marshaller{&Binary{}, &Proto{}, &ProtoingFast{}} {
+	for _, m := range marshallers {
 		for _, keyCount := range []int{10, 100, 10_000, 100_000, 1_000_000, 10_000_000} {
-			runKey := fmt.Sprintf("%d_keys_%T", keyCount, m)
+			runKey := fmt.Sprintf("%d_keys_%s", keyCount, m.name)
 			b.Run(runKey, func(bb *testing.B) {
 				s := &StoreData{Kv: map[string][]byte{}}
 
@@ -41,7 +50,49 @@ func Benchmark_Marshall(b *testing.B) {
 				bb.ReportAllocs()
 
 				for n := 0; n < bb.N; n++ {
-					_, err := m.Marshal(s)
+					_, err := m.m.Marshal(s)
+					require.NoError(bb, err)
+				}
+			})
+		}
+	}
+}
+
+func Benchmark_Unmarshall(b *testing.B) {
+	counter := atomic.NewUint64(0)
+	keyGen := func() string {
+		address1 := make([]byte, 20)
+		binary.LittleEndian.PutUint64(address1, counter.Inc())
+
+		address2 := make([]byte, 20)
+		binary.LittleEndian.PutUint64(address2, counter.Inc())
+
+		return fmt.Sprintf("total:%x:%x", address1, address2)
+	}
+
+	for _, m := range marshallers {
+		for _, keyCount := range []int{10, 100, 10_000, 100_000, 1_000_000, 10_000_000} {
+			runKey := fmt.Sprintf("%d_keys_%s", keyCount, m.name)
+			b.Run(runKey, func(bb *testing.B) {
+				s := &StoreData{Kv: map[string][]byte{}}
+
+				bb.StopTimer()
+				for i := 0; i < keyCount; i++ {
+					s.Kv[keyGen()] = []byte(strconv.FormatInt(int64(i), 10))
+					// Not sure why but trying to `set` 1M keys takes more than 11m!
+					// s.baseStore.set(uint64(i), keyGen(), []byte(strconv.FormatInt(int64(i), 10)))
+				}
+
+				data, err := m.m.Marshal(s)
+				fmt.Println(runKey, len(data))
+				require.NoError(bb, err)
+
+				bb.ResetTimer()
+				bb.StartTimer()
+				bb.ReportAllocs()
+
+				for n := 0; n < bb.N; n++ {
+					_, err := m.m.Unmarshal(data)
 					require.NoError(bb, err)
 				}
 			})

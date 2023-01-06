09d7ee7b763dcb7b832ae367db4340144bd76275
Sebastian Lorenz <fubhy@fubhy.com>
2023-01-06 09:42:56 +0100

    Fix example

--- cmd/substreams/dyntest/src/lib.rs
+++ cmd/substreams/dyntest/src/lib.rs
@@ -5,24 +5,17 @@ use wasmedge_bindgen::*;
 use wasmedge_bindgen_macro::*;
 use crate::pb::custom::{Request, Response};
 use prost::Message;
-use std::error::Error;
 
 #[wasmedge_bindgen]
-pub fn sf_mycustomer_v1_eth_transfers(v: Vec<u8>) -> Result<Vec<u8>, Error> {
-    let req: Request = ::prost::Message::decode(&v[..])?;
+pub fn sf_mycustomer_v1_eth_transfers(v: Vec<u8>) -> Vec<u8> {
+    let req = Request::decode(&v[..]).expect("Failed to decode");
 
     // Integrate calls to `kv_get()` or something
     // that hits the HOST VM (in Go)
 
-    let resp = Response{
+    let resp = Response {
         output: format!("{}{}", req.input, req.input),
     };
 
-    let mut buf = Vec::new();
-    let encoded_len = resp.encoded_len();
-    buf.reserve(encoded_len);
-    match resp.encode(&mut buf) {
-        Ok(_) => Ok(buf),
-        Err(e) => Err(e),
-    }
+    resp.encode_to_vec()
 }

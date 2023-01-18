058ebd1fc6b3237ff85421156883f1b8da714a67
emiliocramer <emilio.cramer101@gmail.com>
2023-01-18 17:06:15 -0500

    support codegen.go for optional manifest

--- cmd/substreams/codegen.go
+++ cmd/substreams/codegen.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/streamingfast/cli"
+	"github.com/streamingfast/substreams/tools"
 	"path"
 	"path/filepath"
 
@@ -12,10 +14,15 @@ import (
 )
 
 var codegenCmd = &cobra.Command{
-	Use:   "codegen <package>",
+	Use:   "codegen [<manifest>]",
 	Short: "Generate a Rust trait and boilerplate code from your 'substreams.yaml' for nicer development",
-	RunE:  runCodeGen,
-	Args:  cobra.ExactArgs(1),
+	Long: cli.Dedent(`
+		Generate a Rust trait and boilerplate code from your 'substreams.yaml' for nicer development. 
+		The manifest is optional as it will try to find one in your pwd if nothing entered.
+		You may enter a dir that contains a 'substreams.yaml' file in place of <manifest_file>
+	`),
+	RunE: runCodeGen,
+	Args: cobra.RangeArgs(0, 1),
 }
 
 func init() {
@@ -23,7 +30,14 @@ func init() {
 }
 
 func runCodeGen(cmd *cobra.Command, args []string) error {
-	manifestPath := args[0]
+	manifestPathRaw := ""
+	if len(args) == 1 {
+		manifestPathRaw = args[0]
+	}
+	manifestPath, err := tools.ResolveManifestFile(manifestPathRaw)
+	if err != nil {
+		return fmt.Errorf("resolving manifest: %w", err)
+	}
 
 	var protoDefinitions []*desc.FileDescriptor
 	manifestReader := manifest.NewReader(manifestPath, manifest.SkipSourceCodeReader(), manifest.WithCollectProtoDefinitions(func(pd []*desc.FileDescriptor) {

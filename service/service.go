fd06963721dd2856c94beae6158bee5f2667d9c7
colindickson <colin.dickson@gmail.com>
2023-01-30 09:32:04 -0500

    cleanup bytes meter setup

--- service/service.go
+++ service/service.go
@@ -157,7 +157,9 @@ func (s *Service) Blocks(request *pbsubstreams.Request, streamSrv pbsubstreams.S
 	hostname := updateStreamHeadersHostname(streamSrv, logger)
 	span.SetAttributes(attribute.String("hostname", hostname))
 
-	s.runtimeConfig.BaseObjectStore.SetMeter(tracking.GetBytesMeter(ctx))
+	if bytesMeter := tracking.GetBytesMeter(ctx); bytesMeter != nil {
+		s.runtimeConfig.BaseObjectStore.SetMeter(bytesMeter)
+	}
 
 	runtimeConfig := config.NewRuntimeConfig(
 		s.runtimeConfig.CacheSaveInterval,

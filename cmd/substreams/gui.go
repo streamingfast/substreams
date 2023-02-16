cb0cc7991800d7dc36511cf042ca5c0a2c78853e
emiliocramer <emilio.cramer101@gmail.com>
2023-02-16 15:15:31 -0500

    add request module view :)

--- cmd/substreams/gui.go
+++ cmd/substreams/gui.go
@@ -187,13 +187,6 @@ func runGui(cmd *cobra.Command, args []string) error {
 		InitialSnapshot: req.DebugInitialStoreSnapshotForModules,
 	}
 	prog := tea.NewProgram(tui2.New(stream, msgDescs, replayLog, &requestSummary), tea.WithAltScreen())
-	requestSummary := requestsummary.RequestSummary{
-		Manifest:        manifestPath,
-		Endpoint:        substreamsClientConfig.Endpoint(),
-		DevMode:         productionMode,
-		InitialSnapshot: req.DebugInitialStoreSnapshotForModules,
-	}
-	prog := tea.NewProgram(tui2.New(stream, msgDescs, replayLog, &requestSummary), tea.WithAltScreen())
 
 	if _, err := prog.Run(); err != nil {
 		return fmt.Errorf("gui error: %w", err)
--- tui2/pages/request/keys.go
+++ tui2/pages/request/keys.go
@@ -2,57 +2,24 @@ package request
 
 import "github.com/charmbracelet/bubbles/key"
 
-// KeyMap defines keybindings. It satisfies to the help.KeyMap interface, which
-// is used to render the menu menu.
-type KeyMap struct {
-	// Help toggle keybindings.
-	ShowFullHelp  key.Binding
-	CloseFullHelp key.Binding
-
-	// The quit keybinding. This won't be caught when filtering.
-	Quit key.Binding
-
-	// The quit-no-matter-what keybinding. This will be caught when filtering.
-	ForceQuit key.Binding
-}
-
-// DefaultKeyMap returns a default set of keybindings.
-func DefaultKeyMap() KeyMap {
-	return KeyMap{
-		// Toggle help.
-		ShowFullHelp: key.NewBinding(
+func (r *Request) ShortHelp() []key.Binding {
+	return []key.Binding{
+		key.NewBinding(
+			key.WithKeys("up", "k", "down", "j"),
+			key.WithHelp("↑/k/↓/j", "up/down"),
+		),
+		key.NewBinding(
 			key.WithKeys("?"),
 			key.WithHelp("?", "more"),
 		),
-		CloseFullHelp: key.NewBinding(
-			key.WithKeys("?"),
-			key.WithHelp("?", "close help"),
-		),
-
-		// Quitting.
-		Quit: key.NewBinding(
+		key.NewBinding(
 			key.WithKeys("q", "esc"),
 			key.WithHelp("q", "quit"),
 		),
-		ForceQuit: key.NewBinding(key.WithKeys("ctrl+c")),
 	}
 }
-
-func (r *Request) ShortHelp() []key.Binding {
-	return []key.Binding{
-		r.KeyMap.ShowFullHelp,
-		r.KeyMap.Quit,
-	}
-}
-
 func (r *Request) FullHelp() [][]key.Binding {
-	k := r.KeyMap
 	return [][]key.Binding{
-		{
-			r.KeyMap.ShowFullHelp,
-		},
-		{
-			k.Quit,
-		},
+		r.ShortHelp(),
 	}
 }

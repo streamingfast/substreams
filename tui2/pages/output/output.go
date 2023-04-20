6383009e5d83f4e0d92a440224b70ed3afbb05a8
emiliocramer <emilio.cramer101@gmail.com>
2023-04-20 13:18:43 -0400

    fix unused import

--- tui2/pages/output/output.go
+++ tui2/pages/output/output.go
@@ -1,7 +1,6 @@
 package output
 
 import (
-	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"

30e46496072fa3b8f014a826ca5794e5a3921b29
Alexandre Bourget <alex@streamingfast.io>
2023-06-29 08:48:21 -0400

    First draft at circling around what needs testing.

--- orchestrator/scheduler/scheduler_test.go
+++ orchestrator/scheduler/scheduler_test.go
@@ -16,6 +16,25 @@ func TestSched2_JobFinished(t *testing.T) {
 	cmd := s.Update(execout.MsgFileDownloaded{})
 	msg := cmd()
 	assert.Equal(t, 1, len(msg.(loop.BatchMsg)))
+	// TODO: implement interfaces on the Scheduler, so it can be easily tested
+	// here and there.
+
+	// Stages:
+	//  * MergeCompleted
+	//  * MarkSegmentPartialPresent
+	//  * NextJob
+	//  * CmdTryMerge
+	//  * WaitAsyncWork
+	//  * CmdStartMerge  (outside of Update loop though)
+	//  * FinalStoreMap  (outside of Update loop though)
+	// WorkerPool:  - No need to interface-ize
+	//  * Borrow / Return
+	// Worker returned here:
+	//  * Work()
+	// ExecOutWalker:
+	//  * CmdDownloadCurrentSegment()
+	//  * NextSegment()
+
 }
 
 func newTestWalker() Walker {

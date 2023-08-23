97ba1aa6af7f71ebf5c163c46feec8e55916a441
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2023-08-22 23:04:39 -0400

    cleanup stream handler for new progress messages

--- orchestrator/response/stream.go
+++ orchestrator/response/stream.go
@@ -33,95 +33,13 @@ func (s *Stream) SendModulesStats(stats []*pbsubstreamsrpc.ModuleStats, stages [
 }
 
 func (s *Stream) RPCFailedProgressResponse(reason string, logs []string, logsTruncated bool) error {
-	// FIXME
-	return nil
-	//return s.respFunc(&pbsubstreamsrpc.Response{
-	//	Message: &pbsubstreamsrpc.Response_Progress{
-	//		Progress: &pbsubstreamsrpc.ModulesProgress{
-	//			Modules: []*pbsubstreamsrpc.ModuleProgress{
-	//				{
-	//					Name: moduleName,
-	//					Type: &pbsubstreamsrpc.ModuleProgress_Failed_{
-	//						Failed: &pbsubstreamsrpc.ModuleProgress_Failed{
-	//							Reason:        reason,
-	//							Logs:          logs,
-	//							LogsTruncated: logsTruncated,
-	//						},
-	//					},
-	//				},
-	//			},
-	//		},
-	//	},
-	//})
+	return s.respFunc(&pbsubstreamsrpc.Response{
+		Message: &pbsubstreamsrpc.Response_FatalError{
+			FatalError: &pbsubstreamsrpc.Error{
+				Reason:        reason,
+				Logs:          logs,
+				LogsTruncated: logsTruncated,
+			},
+		},
+	})
 }
-
-func (s *Stream) RPCRangeProgressResponse(moduleNames []string, start, end uint64) error {
-	// FIXME
-	return nil
-	//var mods []*pbsubstreamsrpc.ModuleProgress
-	//for _, moduleName := range moduleNames {
-	//	mods = append(mods, &pbsubstreamsrpc.ModuleProgress{
-	//		Name: moduleName,
-	//		Type: &pbsubstreamsrpc.ModuleProgress_ProcessedRanges_{
-	//			ProcessedRanges: &pbsubstreamsrpc.ModuleProgress_ProcessedRanges{
-	//				ProcessedRanges: []*pbsubstreamsrpc.BlockRange{
-	//					{
-	//						StartBlock: start,
-	//						EndBlock:   end,
-	//					},
-	//				},
-	//			},
-	//		},
-	//	})
-	//}
-	//return s.respFunc(&pbsubstreamsrpc.Response{
-	//	Message: &pbsubstreamsrpc.Response_Progress{
-	//		Progress: &pbsubstreamsrpc.ModulesProgress{
-	//			Modules: mods,
-	//		},
-	//	},
-	//})
-}
-
-func (s *Stream) RPCProcessedBytes(
-	moduleName string,
-	bytesReadDelta uint64,
-	bytesWrittenDelta uint64,
-	totalBytesRead uint64,
-	totalBytesWritten uint64,
-	nanoSeconds uint64,
-) error {
-	// FIXME
-	return nil
-	//return s.respFunc(&pbsubstreamsrpc.Response{
-	//	Message: &pbsubstreamsrpc.Response_Progress{
-	//		Progress: &pbsubstreamsrpc.ModulesProgress{
-	//			Modules: []*pbsubstreamsrpc.ModuleProgress{
-	//				{
-	//					Name: moduleName,
-	//					Type: &pbsubstreamsrpc.ModuleProgress_ProcessedBytes_{
-	//						ProcessedBytes: &pbsubstreamsrpc.ModuleProgress_ProcessedBytes{
-	//							BytesReadDelta:    bytesReadDelta,
-	//							BytesWrittenDelta: bytesWrittenDelta,
-	//							TotalBytesRead:    totalBytesRead,
-	//							TotalBytesWritten: totalBytesWritten,
-	//							NanoSecondsDelta:  nanoSeconds,
-	//						},
-	//					},
-	//				},
-	//			},
-	//		},
-	//	},
-	//})
-}
-
-/*
-outputstream.Walker
-orchestrator/execout/stream.go Stream
-orchestrator/execout/walker.go Walker
-orchestrator/linear/reader.go Reader
-orchestrator/execout/linearreader.go LinearReader
-orchestrator/execout/walker.go execout.Walker
-
-
-*/

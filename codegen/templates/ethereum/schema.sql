23f6f97b6c203d107d49c94d3274c284c47b0fc7
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2023-10-04 17:40:06 -0400

    adding timestamp_s and block_num to schema.sql generator

--- codegen/templates/ethereum/schema.sql
+++ codegen/templates/ethereum/schema.sql
@@ -2,6 +2,8 @@
 CREATE TABLE IF NOT EXISTS approvals (
     "trx_hash" VARCHAR(40),
     "log_index" INT,
+    "timestamp_s" DECIMAL,
+    "block_num" DECIMAL,
     "approved" VARCHAR(40),
     "owner" VARCHAR(40),
     "token_id" DECIMAL
@@ -9,6 +11,8 @@ CREATE TABLE IF NOT EXISTS approvals (
 CREATE TABLE IF NOT EXISTS approval_for_alls (
     "trx_hash" VARCHAR(40),
     "log_index" INT,
+    "timestamp_s" DECIMAL,
+    "block_num" DECIMAL,
     "approved" BOOL,
     "operator" VARCHAR(40),
     "owner" VARCHAR(40)
@@ -16,12 +20,16 @@ CREATE TABLE IF NOT EXISTS approval_for_alls (
 CREATE TABLE IF NOT EXISTS ownership_transferreds (
     "trx_hash" VARCHAR(40),
     "log_index" INT,
+    "timestamp_s" DECIMAL,
+    "block_num" DECIMAL,
     "new_owner" VARCHAR(40),
     "previous_owner" VARCHAR(40)
 );
 CREATE TABLE IF NOT EXISTS transfers (
     "trx_hash" VARCHAR(40),
     "log_index" INT,
+    "timestamp_s" DECIMAL,
+    "block_num" DECIMAL,
     "from" VARCHAR(40),
     "to" VARCHAR(40),
     "token_id" DECIMAL

2d822dde14552ea2d97e3e626e5dae0ba0d9e4e6
colindickson <colin.dickson@gmail.com>
2024-01-11 10:31:48 -0500

    wasi: refactor code

--- wasm/wasi/module.go
+++ wasm/wasi/module.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"sync"
 
+	"github.com/protocolbuffers/protoscope"
+
 	"github.com/dustin/go-humanize"
 	"github.com/streamingfast/substreams/wasm"
 	"github.com/streamingfast/substreams/wasm/wasi/fs"
@@ -162,4 +164,31 @@ func (w *LogWriter) Write(p []byte) (n int, err error) {
 
 	call.AppendLog(logMessage)
 	return len(p), nil
+}
+
+func marshallArgs(args []wasm.Argument) ([]byte, error) {
+	scopeData := ""
+	fieldCount := 0
+	writeStoreCount := 0
+	readerStoreCount := 0
+	for _, arg := range args {
+		fieldCount++
+		switch v := arg.(type) {
+		case *wasm.StoreWriterOutput:
+			scopeData += fmt.Sprintf("%d: %d\n", fieldCount, writeStoreCount)
+			writeStoreCount++
+		case *wasm.StoreReaderInput:
+			scopeData += fmt.Sprintf("%d: %d\n", fieldCount, readerStoreCount)
+			readerStoreCount++
+		case wasm.ProtoScopeValueArgument:
+			scopeData += fmt.Sprintf("%d: %s\n", fieldCount, v.ProtoScopeValue())
+		default:
+			panic(fmt.Sprintf("unknown wasm argument type %T", v))
+		}
+	}
+	data, err := protoscope.NewScanner(scopeData).Exec()
+	if err != nil {
+		return nil, fmt.Errorf("scanning args: %w (scopeData: %s)", err, scopeData)
+	}
+	return data, nil
 }

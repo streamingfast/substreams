b3e86144cc763c5c9c1e4edd581447c49ed61d52
arnaudberger <arnaudberger84@gmail.com>
2024-04-09 17:30:17 -0400

    Update squasher metrics

--- orchestrator/stage/metrics.go
+++ orchestrator/stage/metrics.go
@@ -3,6 +3,8 @@ package stage
 import (
 	"time"
 
+	"github.com/streamingfast/substreams/block"
+
 	"go.uber.org/zap"
 )
 
@@ -14,6 +16,11 @@ type mergeMetrics struct {
 	mergeEnd   time.Time
 	saveStart  time.Time
 	saveEnd    time.Time
+
+	blockRange *block.Range
+	stage      int
+	moduleName string
+	moduleHash string
 }
 
 func (m mergeMetrics) logFields() []zap.Field {
@@ -31,5 +38,12 @@ func (m mergeMetrics) logFields() []zap.Field {
 	if !m.saveEnd.IsZero() {
 		f = append(f, zap.String("save_time", m.saveEnd.Sub(m.saveStart).String()))
 	}
+
+	if m.blockRange != nil {
+		f = append(f, zap.Uint64("start_block", m.blockRange.StartBlock), zap.Uint64("end_block", m.blockRange.ExclusiveEndBlock))
+	}
+
+	f = append(f, zap.Int("stage", m.stage), zap.String("module_name", m.moduleName), zap.String("module_hash", m.moduleHash))
+
 	return f
 }

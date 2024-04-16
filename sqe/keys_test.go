39bc3e1a9ae832d7d1741996f1105132de660ddf
arnaudberger <arnaudberger84@gmail.com>
2024-04-16 10:31:18 -0400

    Add blockKeys parser

---
+++ sqe/keys_test.go
@@ -0,0 +1,79 @@
+package sqe
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestApplyKeys(t *testing.T) {
+	kv := map[string]struct{}{
+		"bob":      {},
+		"alice":    {},
+		"etienne":  {},
+		"charlie":  {},
+		"delegate": {},
+		"mint":     {},
+	}
+
+	blockKeys := keysQuerier{blockKeys: kv}
+
+	// Matrix-based test cases
+	testCases := []struct {
+		name   string
+		expr   string
+		result bool
+	}{
+		{
+			name:   "Or",
+			expr:   "bob || alice",
+			result: true,
+		},
+		{
+			name:   "And",
+			expr:   "bob transfer",
+			result: false,
+		},
+		{
+			name:   "And(Or key)",
+			expr:   "(alice || bob) transfer",
+			result: false,
+		},
+		{
+			name:   "And(Or Or)",
+			expr:   "(alice || bob) (delegate || mint)",
+			result: true,
+		},
+		{
+			name:   "Not(key)",
+			expr:   "-bob",
+			result: false,
+		},
+		{
+			name:   "And(Or Not(Or))",
+			expr:   "(alice || john) -(delegate || mint)",
+			result: false,
+		},
+		{
+			name:   "2 And",
+			expr:   "alice john mint",
+			result: false,
+		},
+	}
+
+	// Run test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			parser, err := NewParser(strings.NewReader(tc.expr))
+			require.NoError(t, err)
+
+			expr, err := parser.Parse(context.Background())
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.result, blockKeys.apply(expr))
+		})
+	}
+}

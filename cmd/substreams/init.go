45fcb21bcfb3b3f4d42380153768f4209916cd4d
Enol Álvarez <enol826@gmail.com>
2025-02-03 16:05:04 +0100

    WIP

--- cmd/substreams/init.go
+++ cmd/substreams/init.go
@@ -172,8 +172,63 @@ func runSubstreamsInitE(cmd *cobra.Command, args []string) error {
 			return fmt.Errorf("failed to call discovery endpoint: %w", err)
 		}
 
-		var options []huh.Option[*pbconvo.DiscoveryResponse_Generator]
+		type BlockchainProtocolSelector struct {
+			Id         string
+			Title      string
+			Generators []*pbconvo.DiscoveryResponse_Generator
+		}
+		titleFromGroup := func(group string) string {
+			switch group {
+			case "evm":
+				return "EVM"
+			case "solana":
+				return "Solana"
+			case "cosmos":
+				return "Cosmos"
+			case "starknet":
+				return "Starknet"
+			}
+
+			return ""
+		}
+
+		var filteredProtocols map[string]*BlockchainProtocolSelector = make(map[string]*BlockchainProtocolSelector)
 		for _, gen := range resp.Msg.Generators {
+			selector, groupExists := filteredProtocols[gen.Group]
+			if groupExists {
+				selector.Generators = append(selector.Generators, gen)
+			} else {
+				filteredProtocols[gen.Group] = &BlockchainProtocolSelector{
+					Id:         gen.Group,
+					Title:      titleFromGroup(gen.Group),
+					Generators: []*pbconvo.DiscoveryResponse_Generator{gen},
+				}
+			}
+		}
+
+		var protocolOptions []huh.Option[*BlockchainProtocolSelector]
+		for _, value := range filteredProtocols {
+			protocolOptions = append(protocolOptions, huh.Option[*BlockchainProtocolSelector]{
+				Key:   value.Title,
+				Value: value,
+			})
+		}
+
+		var selector *BlockchainProtocolSelector
+		selectField := huh.NewSelect[*BlockchainProtocolSelector]().
+			Title("Choose the protocol you inted to work with:").
+			Options(protocolOptions...).
+			Value(&selector)
+
+		err = huh.NewForm(huh.NewGroup(selectField)).WithTheme(huhTheme).WithAccessible(WITH_ACCESSIBLE).Run()
+		if err != nil {
+			return fmt.Errorf("failed taking input: %w", err)
+		}
+
+		fmt.Println(gray("┃"), "Chosen chain: ", bold(selector.Id), "-", selector.Title)
+
+		var generatorOptions []huh.Option[*pbconvo.DiscoveryResponse_Generator]
+		for _, gen := range selector.Generators {
 			endpoint := ""
 			if gen.Endpoint != "" {
 				endpoint = " (" + gen.Endpoint + ")"
@@ -188,21 +243,23 @@ func runSubstreamsInitE(cmd *cobra.Command, args []string) error {
 				Key:   key,
 				Value: gen,
 			}
-			options = append(options, entry)
+			generatorOptions = append(generatorOptions, entry)
 		}
 
+		fmt.Printf("Testing................")
 		var codegen *pbconvo.DiscoveryResponse_Generator
-		selectField := huh.NewSelect[*pbconvo.DiscoveryResponse_Generator]().
-			Title("Choose the code generator that you want to use to bootstrap your project").
-			Options(options...).
+		selectGeneratorField := huh.NewSelect[*pbconvo.DiscoveryResponse_Generator]().
+			Title("Choose the chain ecosystem that you want to use to bootstrap your project (EVM, Cosmos, Solana, etc)").
+			Options(generatorOptions...).
 			Value(&codegen)
 
-		err = huh.NewForm(huh.NewGroup(selectField)).WithTheme(huhTheme).WithAccessible(WITH_ACCESSIBLE).Run()
+		err = huh.NewForm(huh.NewGroup(selectGeneratorField)).WithTheme(huhTheme).WithAccessible(WITH_ACCESSIBLE).Run()
 		if err != nil {
 			return fmt.Errorf("failed taking input: %w", err)
 		}
 
-		fmt.Println(gray("┃"), "Chosen code generator:", bold(codegen.Id), "-", codegen.Title)
+		fmt.Println(gray("┃"), "Chosen generator: ", bold(codegen.Id), "-", codegen.Title)
+
 		lastState.GeneratorID = codegen.Id
 		generatorID = codegen.Id
 	}

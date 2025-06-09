0f9d8614aaeffe0914a973e334e32863e01c1bd0
UlysseCorbeil <ulysse98@hotmail.com>
2025-06-09 10:37:00 -0400

    revert

--- test/testdata/js_substreams/src/lib.ts
+++ test/testdata/js_substreams/src/lib.ts
@@ -6,8 +6,8 @@ export default class Substreams {
 	@substreams.handlers.map([BlockSchema], MapResultSchema)
 	test_map(blk: Block) {
 		return create(MapResultSchema, {
-			blockNumber: 14n,
-			blockHash: "testdata",
+			blockNumber: blk.number,
+			blockHash: blk.id,
 		});
 	}
 }

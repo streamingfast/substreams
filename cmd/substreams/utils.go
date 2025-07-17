0c199b48910a8f1cbf183f61016af9dbd27cdabf
Matthieu Vachon <matt@streamingfast.io>
2025-07-17 12:23:08 -0400

    Fixed warning in tests

--- cmd/substreams/utils.go
+++ cmd/substreams/utils.go
@@ -90,7 +90,7 @@ func warnIncompletePackage(spkg *pbsubstreams.Package, config warningsConfig) (w
 
 	if len(warnings) > 0 {
 		fmt.Println()
-		fmt.Printf(config.Indent + "⚠️ Detected Substreams Package warnings:\n")
+		fmt.Println(config.Indent + "⚠️ Detected Substreams Package warnings:")
 		for _, warning := range warnings {
 			fmt.Printf(config.Indent+"   • %s\n", warning)
 		}

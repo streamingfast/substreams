d12d7015be6f50d6bc0fb3e51b5c8ac197f90adb
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2025-09-03 15:22:06 -0400

    add to logs the 'client_read_average_time_last_5_minutes' to see if they are the cause of the wait time

--- service/tier1.go
+++ service/tier1.go
@@ -898,10 +898,12 @@ func tier1ResponseHandler(ctx context.Context, mut *sync.Mutex, logger *zap.Logg
 			}
 		}
 
+		begin := time.Now()
 		if err := streamSrv.Send(resp); err != nil {
 			logger.Info("unable to send block probably due to client disconnecting", zap.String("user_id", userID), zap.String("api_key_id", apiKeyID), zap.Error(err))
 			return connect.NewError(connect.CodeUnavailable, err)
 		}
+		stats.RecordReadTime(begin)
 
 		if isData {
 			stats.RecordDataSent()

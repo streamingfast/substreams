ed958e93cbf11aa78794d5508ae9c2b67e101d1b
Charles Billette <charles@cbillette.com>
2026-02-10 14:17:19 -0500

    Remove vtprotobuf gRPC codec import and associated registration code

--- client/client.go
+++ client/client.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/dustin/go-humanize"
 	"github.com/mostynb/go-grpc-compression/experimental/s2"
-	vt "github.com/planetscale/vtprotobuf/codec/grpc"
 	"github.com/streamingfast/dgrpc"
 	networks "github.com/streamingfast/firehose-networks"
 	"github.com/streamingfast/logging/zapx"
@@ -25,16 +24,15 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/credentials/oauth"
 	xdscreds "google.golang.org/grpc/credentials/xds"
-	"google.golang.org/grpc/encoding"
 	_ "google.golang.org/grpc/encoding/gzip"
 	stats "google.golang.org/grpc/stats"
 	_ "google.golang.org/grpc/xds"
 )
 
-func init() {
-	fmt.Println("------------------ VT Proto registered ------------------------")
-	encoding.RegisterCodec(vt.Codec{})
-}
+//func init() {
+//	fmt.Println("------------------ VT Proto registered ------------------------")
+//	encoding.RegisterCodec(vt.Codec{})
+//}
 
 type AuthType int
 

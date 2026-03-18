f5203c73df712faef8e60acec978cc600bd768f3
Matthieu Vachon <matt@streamingfast.io>
2026-03-18 19:37:22 -0400

    refactor: replace userID with organizationID in session handling

--- service/tier1.go
+++ service/tier1.go
@@ -809,12 +809,12 @@ func (s *Tier1Service) blocks(
 
 	if s.sessionPool != nil {
 		auth := dauth.FromContext(ctx)
-		userID := auth.UserID()
+		organizationID := auth.OrganizationID()
 		apiKeyID := auth.APIKeyID()
 		traceID := tracing.GetTraceID(ctx).String()
 		service := "t1r"
 
-		sessionID, err := s.sessionPool.Get(ctx, service, userID, apiKeyID, traceID, func(err error) {
+		sessionID, err := s.sessionPool.Get(ctx, service, organizationID, apiKeyID, traceID, func(err error) {
 			if cancelRunning != nil { // in tests, this might be nil
 				cancelRunning(err)
 			}
@@ -825,9 +825,9 @@ func (s *Tier1Service) blocks(
 			case errors.Is(err, dsession.ErrConcurrentStreamLimitExceeded),
 				errors.Is(err, dsession.ErrPermissionDenied),
 				errors.Is(err, dsession.ErrQuotaExceeded):
-				s.logger.Info("session denied to user", zap.String("user_id", userID), zap.String("api_key_id", apiKeyID), zap.String("trace_id", traceID), zap.Error(err))
+				s.logger.Info("session denied to user", zap.String("organization_id", organizationID), zap.String("api_key_id", apiKeyID), zap.String("trace_id", traceID), zap.Error(err))
 			default:
-				s.logger.Error("failed to acquire session", zap.Error(err), zap.String("service", service), zap.String("user_id", userID), zap.String("api_key_id", apiKeyID), zap.String("trace_id", traceID))
+				s.logger.Error("failed to acquire session", zap.Error(err), zap.String("service", service), zap.String("organization_id", organizationID), zap.String("api_key_id", apiKeyID), zap.String("trace_id", traceID))
 			}
 			return err
 		}

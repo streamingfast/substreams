836046caeae9bdaa3341c64ab6243467325c7506
0xGabey <cartier.gabriel@gmail.com>
2026-07-22 17:39:15 +0700

    refactor(sink/sql): register postgres driver in the db package
    The package opens connections itself; importers should not need a
    blank lib/pq import. Mirrors #761.

--- cmd/substreams/sink_sql_common.go
+++ cmd/substreams/sink_sql_common.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/desc/protoparse"
-	_ "github.com/lib/pq"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/streamingfast/cli"
